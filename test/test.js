6d9cee563eee1a6cdfc56de14908c16f145a7452
syzer3@gmail.com <syzer3@gmail.com>
2016-09-29 10:08:02 +0200

    renamed test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ import test from 'ava'
 import latency from '../routes/latency'
 import _ from 'lodash'
 
-test('bar', async t => {
+test('getLatencyData', async t => {
     const out = latency.getData('http://localhost:3000/')
 
     t.truthy(_.isArray(await out))
